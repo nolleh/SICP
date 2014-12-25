0a5be6dcebbb3eca07eaea68e4f9c35daadff2cb
nolleh <nolleh7707@gmail.com>
2014-12-26 00:24:05 +0900

    remaining filtered accumulate's one version, and passing the bi-predicate as unary, by using binding the argumemts. / modify function to actually iterative process, not recursive process.

--- src/1.8.exercise_1.33.hs
+++ src/1.8.exercise_1.33.hs
@@ -1,5 +1,6 @@
---main = print $ filtered_accumulate' prime plus 0 square 1 inc 5
-main = print $ filtered_accumulate'' relative_prime multiply 1 identity 1 inc 5
+main = do 
+  print $ filtered_accumulate prime plus 0 square 1 inc 4
+  print $ filtered_accumulate (relative_prime 5) multiply 1 identity 1 inc 5
 identity a = a
 square a = a * a
 inc a = a + 1
@@ -14,20 +15,20 @@ prime a =
 		          | square test_divisor > n = n
 		          | rem n test_divisor == 0 = test_divisor
 		          | otherwise = find_divisor n (test_divisor + 1)
-relative_prime i n
+relative_prime n i
   | 0 < i && i < n && gcd i n == 1 = True
   | otherwise = False
 
-filtered_accumulate' predicate combiner null_value term a next b = 
+filtered_accumulate predicate combiner null_value term a next b = 
   iter a null_value
     where iter a result
-  		    | a > b = null_value
-  		    | predicate a == True = iter (next a) result `combiner` (term a)
+  		    | a > b = result
+  		    | predicate a == True = iter (next a) (combiner result (term a))
   		    | otherwise = iter (next a) result
 
-filtered_accumulate'' bi_predicate combiner null_value term a next b = 
+filtered_accumulate' bi_predicate combiner null_value term a next b = 
   iter a null_value
     where iter a result
-  		    | a > b = null_value
-  		    | bi_predicate a b == True = iter (next a) result `combiner` (term a)
+  		    | a > b = result
+  		    | bi_predicate a == True = iter (next a) (combiner result (term a))
   		    | otherwise = iter (next a) result
