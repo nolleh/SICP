cca01fe444b91a542d8f2aae965824c341122ced
nolleh <nolleh@neowiz.com>
2015-05-18 14:30:05 +0900

    subsets

---
+++ src/chap.2/2.32.subsets.hs
@@ -0,0 +1,17 @@
+-- 2.32. subsets
+-- (), (3), (2), (2, 3), (1), (1,3), (1,2), (1,2,3)
+
+-- not work.
+subsets [] = []
+subsets (x:xs) = 
+	let rest = subsets xs
+	in rest ++ (map (x:) rest)
+
+--subsets (x:xs) = 
+--	map (:1) xs
+-- xs : 2, 3 -> [[1, 2] [1, 3]]
+--- want to out list's list... same haskell problem..
+main = do
+	print $ subsets [1,2,3]
+
+-- append (car s) rest
