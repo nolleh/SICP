ddd36f68535ff93757692f62484470096313aff0
nolleh <nolleh@neowiz.com>
2015-05-18 14:39:00 +0900

    exercies subsets, 2.32

--- src/chap.2/2.32.subsets.hs
+++ src/chap.2/2.32.subsets.hs
@@ -1,8 +1,7 @@
 -- 2.32. subsets
 -- (), (3), (2), (2, 3), (1), (1,3), (1,2), (1,2,3)
 
--- not work.
-subsets [] = []
+subsets [] = [[]]
 subsets (x:xs) = 
 	let rest = subsets xs
 	in rest ++ (map (x:) rest)
