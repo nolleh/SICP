058b8235c6bd5b7bb53120f1bd0edbf961b12799
nolleh <nolleh@neowiz.com>
2015-05-18 14:40:10 +0900

    add output

--- src/chap.2/2.32.subsets.hs
+++ src/chap.2/2.32.subsets.hs
@@ -12,5 +12,5 @@ subsets (x:xs) =
 --- want to out list's list... same haskell problem..
 main = do
 	print $ subsets [1,2,3]
-
+-- [[],[3],[2],[2,3],[1],[1,3],[1,2],[1,2,3]]
 -- append (car s) rest
