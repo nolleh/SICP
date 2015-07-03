378c6032c89ef7b0ce88f254f0f39263d009ad36
nolleh <nolleh@neowiz.com>
2015-07-03 17:19:26 +0900

    bugfix

--- src/chap.2/2.51.below.scm
+++ src/chap.2/2.51.below.scm
@@ -20,13 +20,13 @@
     (let ((paint-lower
            (transform-painter painter1
                               (make-vect 0.0 0.0)
-                              (make-vect 0.0 1.0)
+                              (make-vect 1.0 0.0)
                               split-point))
           (paint-upper
            (transform-painter painter2
                               split-point
-                              (make-vect 0.0 1.0)
-                              (make-vect 0.0 0.5))))
+                              (make-vect 1.0 0.0)
+                              (make-vect 0.0 1.0))))
       (lambda (frame)
         (paint-left frame)
         (paint-right frame)))))
@@ -34,4 +34,6 @@
 
 ; 2
 (define (below painter1 painter2)
-  (rotate90 (beside painter1 painter2)))
+  (let (lower (rotate90 painter1)))
+  (let (higher (rotate90 painter2)))
+  (rotate270 (beside (lower higher))))
